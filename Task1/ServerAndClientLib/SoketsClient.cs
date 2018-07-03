87a0ff9ee3e134ff186d204c7b31d2b09c0f4451
Feasad <Feasad@SERGEY>
2018-07-03 10:35:56 +0400

    tmp commit

---
+++ Task1/ServerAndClientLib/SoketsClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerAndClientLib
+{
+    public class SoketsClient
+    {
+        public string Id { get; private set; }
+        public NetworkStream Stream { get; private set; }
+        public string userName;
+        public TcpClient client;
+        public SoketsServer server;
+
+        public SoketsClient(TcpClient client, SoketsServer server)
+        {
+            Id = Guid.NewGuid().ToString();
+            this.client = client;
+            this.server = server;
+            server.AddConnection(this);
+        }
+    }
+}
---
+++ Task1/ServerAndClientLib/SoketsServer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerAndClientLib
+{
+    public class SoketsServer
+    {
+        public TcpListener tcpListener; 
+        public List<SoketsClient> clients = new List<SoketsClient>();
+
+        protected internal void AddConnection(SoketsClient client)
+        {
+            clients.Add(client);
+        }
+    }
+}
